2adde17e465cc44f429512f8fd0fa94e2e264822
Talha Manj <talha.manj@tuni.fi>
2020-11-23 21:41:00 +0400

    10.3 Implemented

---
+++ test/own/own.test.js
@@ -0,0 +1,22 @@
+const chai = require("chai");
+
+
+describe('Products', () => {
+    describe('Products', () => {
+        it('Should return all Products', async () => {})
+        it('Should return all Products', async () => {})
+        it('Should return all Products', async () => {})
+        it('Should return all Products', async () => {})
+    })
+    describe('Products', () => {
+        it('Should return all Products', async () => {})
+        it('Should return all Products', async () => {})
+        it('Should return all Products', async () => {})
+        it('Should return all Products', async () => {})
+    })
+    describe('Products', () => {
+        it('Should return all Products', async () => {})
+        it('Should return all Products', async () => {})
+        it('Should return all Products', async () => {})
+    });
+});
