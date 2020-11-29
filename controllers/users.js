622b8e1ecc34747a0c6d3b9463e805d68178e366
Talha Manj <talha.manj@tuni.fi>
2020-11-29 22:54:15 +0500

    Fix merge part in users.js

--- controllers/users.js
+++ controllers/users.js
@@ -60,13 +60,8 @@ const updateUser = async (response, userId, currentUser, userData) => {
 	else if (!userData.role || (userData.role !== 'customer' && userData.role !== 'admin')) {
 		return responseUtils.badRequest(response, "Bad Request");
 	}
-<<<<<<< HEAD
-	else if (userId == currentUser.id) {
-		return responseUtils.badRequest(response, "Updating own data is not allowed");
-=======
-	if(currentUser.role !== 'admin'){
+	else if(currentUser.role !== 'admin'){
 		return responseUtils.forbidden(response); 
->>>>>>> a11eb825cf58c6d5299dd5778fa9bf321ef12c54
 	}
 	else if(currentUser.role == 'admin'){
 		let user = await User.findById(userId).exec();
@@ -94,15 +89,9 @@ const viewUser = async (response, userId, currentUser) => {
   // if(!onload) return notFound(response);
   // return sendJson(response, onload);
   if(currentUser.role !== 'admin'){
-<<<<<<< HEAD
 		return forbidden(response); 
     }
 	else if(currentUser.role == 'admin'){
-=======
-		return responseUtils.forbidden(response); 
-  }
-	if(currentUser.role == 'admin'){
->>>>>>> a11eb825cf58c6d5299dd5778fa9bf321ef12c54
 		const user = await User.findById(userId).exec();
 		if (!user) {
 			return responseUtils.notFound(response);
