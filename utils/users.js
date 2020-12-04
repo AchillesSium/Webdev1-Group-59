ee350d7024e1ffc8a71eaa3126551ac81b05a3ad
Mohammad Aqib Hassan <mohammad.hassan@tuni.fi>
2020-12-04 18:14:21 +0200

    11.3.4 solved

--- utils/users.js
+++ utils/users.js
@@ -38,15 +38,16 @@ const demoUsers = data.users;
  * @returns {string} return a string
  */
 const generateId = () => {
-  let id;
+  var id;
 
-  do {
+  //do {
     // Generate unique random id that is not already in use
     // Shamelessly borrowed from a Gist. See:
     // https://gist.github.com/gordonbrander/2230317
 
     id = Math.random().toString(36).substr(2, 9);
-  } while (data.users.some(u => u._id === id));
+    id = getRandomId(id)
+  //} while (data.users.some(u => u._id === id));
 
   return id;
 };
